ace2c4460417ce2c0b07f945b633732b934600f2
Scareyy <scareyy5301@gmail.com>
2026-07-10 02:53:31 +0530

    feat(terraform): enable SSM access and provision EC2 deployment target

--- terraform/modules/security-group/main.tf
+++ terraform/modules/security-group/main.tf
@@ -3,6 +3,16 @@ resource "aws_security_group" "app_sg" {
   description = "Security group for application EC2"
   vpc_id      = var.vpc_id
 
+  egress {
+  description = "Allow all outbound traffic"
+
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
   tags = {
     Name = "${var.project_name}-app-sg"
   }
